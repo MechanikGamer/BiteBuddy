8f2204326b7a0c2621e475d57a83097d05b21246
MechanikGamer <123777101+MechanikGamer@users.noreply.github.com>
2024-09-04 19:17:45 +0200

    Fix

--- apps/BiteBuddy/metro.config.js
+++ apps/BiteBuddy/metro.config.js
@@ -13,11 +13,6 @@ const customConfig = {
     assetExts: assetExts.filter((ext) => ext !== 'svg'),
     sourceExts: [...sourceExts, 'cjs', 'mjs', 'svg'],
   },
-  watchFolders: [
-    // Add paths to additional folders if needed
-    `${__dirname}/../../../../assets`,
-    `${__dirname}/../../utility`,
-  ],
 };
 
 module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
